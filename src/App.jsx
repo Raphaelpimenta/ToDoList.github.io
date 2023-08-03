1f93a22adf755b2b8d19a3ded10a1e6829f12240
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-08-03 05:40:42 -0300

    ajuste no caminho do arquivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import Todo from "./components/todo";
+import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import Search from './components/Search';
 
